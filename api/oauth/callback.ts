19823bcce71ee8735b20da6ea1c002c13b612c42
Con Co <32479659+conco1236@users.noreply.github.com>
2026-08-29 18:56:19 +0700

    Delete api/oauth/callback.ts

--- api/oauth/callback.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerOAuthRoutes } from "../../server/_core/oauth.js";
-
-const app = express();
-app.use(express.json({ limit: "1mb" }));
-registerOAuthRoutes(app);
-
-export default app;
